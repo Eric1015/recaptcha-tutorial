e8b90bae8195a5c7d970578ce515b2c6c4e60349
love1 <love1dbsb77@hotmail.com>
2018-07-21 13:05:17 -0700

    Added verificaiton for recaptcha

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if verify_recaptcha(model: @user) || @user.save
+    if verify_recaptcha(model: @user) && @user.save
       redirect_to thanks_path
     else
       redirect_to root_path
